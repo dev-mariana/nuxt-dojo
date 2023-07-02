ff9f8a85d8fc70e400e9a4f38eedeb4fede653a1
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2023-07-01 23:18:18 -0300

    feat: add product route

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>teste</p>
+    <p>teste</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 20px 0;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
