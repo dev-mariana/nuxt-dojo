a1c5c1313504f6ae631df3e57c0f8c5ae9f38f5e
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2023-07-01 23:29:46 -0300

    feat: add nuxtLink and navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">Products</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <h2>Home</h2>
     <p>teste</p>
     <p>teste</p>
