38494441418a76651c226f84b1e92f6c977f39f5
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2023-07-01 23:43:09 -0300

    feat: add layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,5 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
-        <ul>
-          <li>
-            <NuxtLink to="/">Home</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/about">About</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/products">Products</NuxtLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
     <h2>Home</h2>
     <p>teste</p>
     <p>teste</p>
