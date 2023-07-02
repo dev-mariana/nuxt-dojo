9735b36b5863a843ca5795271998211db3816566
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2023-07-01 23:21:04 -0300

    feat: add detail(id) page from products

---
+++ pages/products/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Product Id {{ id }}</p>
+    <p>teste</p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
