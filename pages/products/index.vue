a2cf17667138537e492d1e96f1e8200afe9bbf52
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2023-07-02 17:03:53 -0300

    feat: add productCard and productDetails components

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,18 +2,18 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="product in products">
-        <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+        <ProductCard :product="product" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+
 definePageMeta({
   layout: "products",
 });
-
-const { data: products } = await useFetch("https://fakestoreapi.com/products");
 </script>
 
 <style lang="scss" scoped></style>
