556bcfaf1a792b461150b0775ab08163a48427bb
Mariana Bastos <maribastossrodrigues@gmail.com>
2023-07-02 18:03:30 -0300

    feat: add metada and useHead

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <head>
+      <Title>Nuxt Dojo | {{ product.title }}</Title>
+      <Meta name="description" :content="product.description" />
+    </head>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="product in products">
         <ProductCard :product="product" />
@@ -9,10 +13,15 @@
 </template>
 
 <script setup>
-const { data: products } = await useFetch("https://fakestoreapi.com/products");
-
 definePageMeta({
   layout: "products",
+});
+
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+
+useHead({
+  title: "Nuxt Dojo | Merch",
+  meta: [{ name: "description", content: "Nuxt 3 Merch" }],
 });
 </script>
 
